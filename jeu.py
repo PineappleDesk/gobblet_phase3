993f736cf362ccdbeb097a342b6d3ceea38f94ce
PineappleDesk <imane.baqri.1@ulaval.ca>
2022-05-03 00:28:49 +0000

    Module Jeu second Commit

---
+++ jeu.py
@@ -0,0 +1,13 @@
+class Jeu:
+    def __init__(self):
+        
+        self.idul = ""
+        self.secret = ""
+        self.id_partie = None
+        self.automatique = False
+
+    def __str__(self):
+        pass
+
+    def jouer():
+        pass
