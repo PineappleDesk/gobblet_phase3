dba2d24e7aa8db13b7dedb7d4e6a8dbc8cdd045d
PineappleDesk <imane.baqri.1@ulaval.ca>
2022-05-03 01:42:00 +0000

    str and constructor's commit

--- jeu.py
+++ jeu.py
@@ -1,13 +1,41 @@
-class Jeu:
-    def __init__(self):
-        
-        self.idul = ""
-        self.secret = ""
-        self.id_partie = None
-        self.automatique = False
+import joueur
+import api
+import gobblet
 
-    def __str__(self):
-        pass
+class Jeu:
+    def __init__(self, idul, secret, id_partie=None, automatique=False):
+        self.plateau = plateau
+        if automatique :
+            self.joueur1 = Automate(Joueur)
+        else :
+            self.joueur1 = Joueur()
+        self.joueur2 = Joueur()
+        self.secret = ""
+        self.id_partie = ""
+
+        try:
+            api.lister_parties(idul, secret)
+
+        except PermissionError():
+            raise GobbletError("L'IDUL {idul} n'est pas reconnu par le serveur.")
+
+        if id_partie :
+            try :
+                api.récupérer_partie(id_partie, idul, secret)
+            except RuntimeError():
+                raise GobbletError("L'identifiant {id_partie} ne correspond pas à une partie du joueur {idul}.")
+
+
+
+
+    def __str__(self, plateau, joueurs):
+        gobblet.formater_jeu(plateau, joueurs)
 
     def jouer():
-        pass
+
+        while True:
+            tour = 1
+            if tour == 1 :
+                tour = 2
+            if tour == 2 :
+                tour = 1
